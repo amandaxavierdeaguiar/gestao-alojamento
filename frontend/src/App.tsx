cba0177c62caa2937f93ea68db04b3bcab5ffe62
amandaxavierdeaguiar <amandaxavierdeaguiar@gmail.com>
2026-05-25 21:10:57 +0100

    feat: Inserted routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import MasterLayout from './layout/LayoutDashboard';
+import Dashboard from './pages/dashboard-home/DashboardHome';
+
+import DashboardGuestAdd from './pages/dashboard-guest/DashboardGuestAdd';
+
 function App() {
   return (
-    <>
-      START PROJECT!
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Rota Pai: O Layout Master abraça as páginas internas */}
+        <Route path="/" element={<MasterLayout />}>
+          
+          {/* Rota Padrão: Se o usuário acessar "/", ele é redirecionado para "/dashboard" */}
+          <Route index element={<Navigate to="/dashboard" replace />} />
+          
+          {/* Rota Filha: O Dashboard será injetado dentro do <Outlet /> do Layout */}
+          <Route path="dashboard" element={<Dashboard />} />
+          
+          <Route path="add-guest" element={<DashboardGuestAdd />} />          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
